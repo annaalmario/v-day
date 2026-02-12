65df8d949ca7cf7fc070b9a3eb289f8904c1da5a
annaalmario <80401114+annaalmario@users.noreply.github.com>
2026-02-13 00:01:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
+    "https://tenor.com/view/love-gif-17552840805493029515",    // 0 normal
+    "https://tenor.com/view/chiikawa-gif-4969396691811465935",  // 1 confused
     "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
+    "https://tenor.com/view/crying-cry-chiikawa-cute-gif-14955844422678245503",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
